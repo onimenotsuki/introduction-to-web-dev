7ab9bff95e5a8617ec78a01a781849d40adf1f6d
Edgar Talledos <onimenotsuki@gmail.com>
2017-10-24 11:12:50 -0500

    Install dependencies and add dotenv

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const engine = require('consolidate');
-
 const index = require('./routes/index');
 
 const app = express();
@@ -23,7 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join('dist')));
 
 app.use('/', index);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+require('dotenv').config();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: `${ process.env.HELLO  } Express` });
 });
 
 module.exports = router;
