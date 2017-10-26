1ac34a8e1568c5983a33af49dcaff917a7e6186e
Edgar Talledos <onimenotsuki@gmail.com>
2017-10-25 21:21:12 -0500

    FIX: error with syntax

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const engine = require('consolidate');
 const index = require('./routes/index');
-const privacy = require('./routes/privacy');
+const privacy = require('./routes/privacy');
 
 const app = express();
 
